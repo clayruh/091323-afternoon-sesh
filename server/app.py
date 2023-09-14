242520625dbd8c1e799ed14ac620710184c0b724
Clae <56447997+clayruh@users.noreply.github.com>
2023-09-14 10:12:40 -0400

    set up project

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Hero, Villain, HeroVillain
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,7 +19,30 @@ db.init_app(app)
 def index():
     return "Hello world"
 
-# write your routes here!
+@app.get('/debug')
+def debug():
+    import ipdb; ipdb.set_trace()
+    return "Debugging! Yay!"
+
+@app.get('/heroes')
+def all_heroes():
+    heroes = Hero.query.all()
+    # here we've defined to_dict() function
+    return [hero.to_dict() for hero in heroes], 200
+
+@app.get('/heroes/<int:id>')
+def hero_by_id(id: int):
+    try: 
+        hero = Hero.query.filter(Hero.id == id).first()
+        response = hero.to_dict()
+        return response, 200
+    except Exception as e:
+        return {'error': "404 not found"}, 404
+
+@app.get('/villains')
+def all_villains():
+    villains = Villain.query.all()
+    return [villain.to_dict() for villain in villains], 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,4 +10,66 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# write your models here!
+# even though db.Model and SerializerMixin have the same functions, the second inheritance overwrites
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    power = db.Column(db.String)
+    weakness = db.Column(db.String)
+
+    herovillains = db.relationship("HeroVillain", back_populates='hero')
+    # want to be able to write hero1.villains and show all the associations
+    # villains = association_proxy('the middle bridge', 'relationship variable')
+    villains = association_proxy('herovillains', 'villain')
+
+    # prevent recursion, so use serialize_rules ('- do not give me this!'). takes iterable, so list or tuple. don't bounce back '-herovillains.SELF'
+    serialize_rules = ('-heroesvillains.hero',)
+
+    # def to_dict(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "power": self.power,
+    #         "weakness": self.weakness,
+    #         # make a dictionary of each villain attached to hero
+    #         "villains": [villain.to_dict() for villain in self.villains]
+    #     }
+
+class Villain(db.Model, SerializerMixin):
+    __tablename__ = 'villains'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    secret_lair = db.Column(db.String)
+    childhood_trauma = db.Column(db.String)
+
+    herovillains = db.relationship("HeroVillain", back_populates="villain")
+    heroes = association_proxy('herovillains', 'hero')
+
+    serialize_rules = ('-herotovillains.villain', )
+
+    # def to_dict_with_heroes(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "heroes": [hero.to_dict() for hero in self.heroes]
+    #     }
+    
+    # def to_dict(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #     }
+
+class HeroVillain(db.Model, SerializerMixin):
+    __tablename__ = 'heroesvillains'
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    villain_id = db.Column(db.Integer, db.ForeignKey('villains.id'))
+
+    hero = db.relationship("Hero", back_populates='herovillains')
+    villain = db.relationship("Villain", back_populates='herovillains')
+
+    serialize_rules = ('-hero.herovillains', '-.villain.herovillains')
